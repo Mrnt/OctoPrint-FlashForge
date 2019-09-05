6bdfb35de543fe27186ea8a30b7fcd9e08d3dbd8
Mrnt <it@maurent.com>
2019-09-05 07:27:25 -0700

    Install libusb1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/Mrnt/OctoPrint-FlashForge"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ['libusb1']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
