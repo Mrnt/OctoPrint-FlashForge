a963cfcddf7a4b5cb30c2c9066e8f66e72926067
Peter <pfemiani@users.noreply.github.com>
2020-03-29 11:16:05 -0700

    Adding Creator Max

--- octoprint_flashforge/__init__.py
+++ octoprint_flashforge/__init__.py
@@ -17,7 +17,7 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 	PRINTER_IDS = {
 		"PowerSpec": {0x0001: "Ultra 3DPrinter"},
 		"Dremel": {0x8889: "Dremel IdeaBuilder"},
-		"FlashForge": {0x0001: "Dreamer", 0x000A: "Dreamer NX", 0x0002: "Finder v1", 0x0005: "Inventor", 0x0007: "Finder v2", 0x00ff: "PowerSpec Ultra"}}
+		"FlashForge": {0x00e7: "Creator Max", 0x0001: "Dreamer", 0x000A: "Dreamer NX", 0x0002: "Finder v1", 0x0005: "Inventor", 0x0007: "Finder v2", 0x00ff: "PowerSpec Ultra"}}
 	FILE_PACKET_SIZE = 1024
 
 
