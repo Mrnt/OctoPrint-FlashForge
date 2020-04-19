558933ea1df75d12b34ebce599cf74898c46f114
Mrnt <it@maurent.com>
2020-04-18 19:39:25 -0700

    Adding more detail to troubleshooting permissions problem

--- octoprint_flashforge/flashforge.py
+++ octoprint_flashforge/flashforge.py
@@ -59,8 +59,19 @@ class FlashForge(object):
 		except usb1.USBError as usberror:
 			if usberror.value == -3:
 				raise FlashForgeError("Unable to connect to FlashForge printer - permission error.\r\n\r\n"
-									  "On OctoPi/Linux add the following line to\r\n /etc/udev/rules.d/99-octoprint.rules:\r\n\r\n"
-									  "SUBSYSTEM==\"usb\", ATTR{{idVendor}}==\"{:04x}\", MODE=\"0666\"\r\n\r\nThen reboot your system for the rule to take effect.\r\n\r\n".format(vendor_id))
+									  "If you are using OctoPi/Linux add permission to access this device by editing file:\r\n /etc/udev/rules.d/99-octoprint.rules\r\n\r\n"
+									  "and adding the line:\r\n"
+									  "SUBSYSTEM==\"usb\", ATTR{{idVendor}}==\"{:04x}\", MODE=\"0666\"\r\n\r\n"
+									  "You can do this as follows:\r\n"
+									  "1) Connect to your OctoPi/Octoprint device using ssh\r\n"
+									  "2) Type the following to open a text editor:\r\n"
+									  "sudo nano /etc/udev/rules.d/99-octoprint.rules\r\n"
+									  "3) Add the following line:\r\n"
+  									  "SUBSYSTEM==\"usb\", ATTR{{idVendor}}==\"{:04x}\", MODE=\"0666\"\r\n\r\n"
+									  "4) Save the file and close the editor\r\n"
+									  "5) Verify the file permissions are set to \"rw-r--r--\" by typing:\r\n"
+									  "ls /etc/udev/rules.d/99-octoprint.rules"
+									  "6) Reboot your system for the rule to take effect.\r\n\r\n".format(vendor_id, vendor_id))
 			else:
 				raise FlashForgeError('Unable to connect to FlashForge printer - may already be in use', usberror)
 		else:
