5e64d8d6f4804d033941813cfac3a614856803bc
Mrnt <it@maurent.com>
2020-09-14 14:20:37 -0700

    - disable keep alive when uploading files (shouldn't really matter because there's already blocking on read write) - get status before M27 to make sure we return the correct SD print status which should fix bug where print controls dont work after an upload to SD card - code cleanup

--- octoprint_flashforge/__init__.py
+++ octoprint_flashforge/__init__.py
@@ -17,16 +17,15 @@ G91 (relative positioning) appears not to be supported by Finder 2, Guider 2 (F2
 - F2G2 does not appear to support "G28 X Y"
 '''
 
+
 class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin,
-                       octoprint.plugin.TemplatePlugin):
-
-
+					   octoprint.plugin.AssetPlugin,
+					   octoprint.plugin.TemplatePlugin):
 	VENDOR_IDS = {0x0315: "PowerSpec", 0x2a89: "Dremel", 0x2b71: "FlashForge"}
 	PRINTER_IDS = {
 		"PowerSpec": {0x0001: "Ultra 3DPrinter (C)"},
 		"Dremel": {0x8889: "Dremel IdeaBuilder 3D20", 0x888d: "Dremel IdeaBuilder 3D45"},
-		"FlashForge": {0x0001: "Dreamer", 0x0002: "Finder v1",  0x0004: "Guider II", 0x0005: "Inventor",
+		"FlashForge": {0x0001: "Dreamer", 0x0002: "Finder v1", 0x0004: "Guider II", 0x0005: "Inventor",
 					   0x0007: "Finder v2", 0x0009: "Guider IIs", 0x000A: "Dreamer NX",
 					   0x00e7: "Creator Max", 0x00ee: "Finder v2.12",
 					   0x00f6: "PowerSpec Ultra 3DPrinter (B)", 0x00ff: "PowerSpec Ultra 3DPrinter (A)"}}
@@ -56,6 +55,8 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 		}
 		default_settings["serial"] = dict_merge(default_settings["serial"], self._conn_settings)
 		default_settings["feature"] = dict_merge(default_settings["feature"], self._feature_settings)
+
+		self._logger.info("libusb1: {}".format(usb1.__version__))
 
 
 	##~~ SettingsPlugin mixin
@@ -121,9 +122,15 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 					device_name = device.getProduct()
 				except:
 					device_name = 'unknown'
-				self._logger.debug("Found device '{}' with Vendor ID: {:#06X}, USB ID: {:#06X}".format(device_name, vendor_id, device_id))
+				self._logger.debug(
+					"Found device '{}' with Vendor ID: {:#06X}, USB ID: {:#06X}".format(device_name, vendor_id,
+																						device_id))
 
 				if vendor_id in self.VENDOR_IDS:
+					if device_name == "unknown":
+						# we have a printer but something wrong with it or the USB system
+						raise flashforge.FlashForgeError("Found printer but there is a connection problem - reboot printer.")
+						continue
 					vendor_name = self.VENDOR_IDS[vendor_id]
 					self._logger.info("Found a {} {}".format(vendor_name, self.PRINTER_IDS[vendor_name][device_id]))
 					self._printers[device_name] = {'vid': vendor_id, 'vname': vendor_name, 'did': device_id}
@@ -133,13 +140,13 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 							for setting in interface:
 								self._logger.debug(
 									" setting number: 0x{:02x}, class: 0x{:02x}, subclass: 0x{:02x}, protocol: 0x{:02x}, #endpoints: {}, descriptor: {}".format(
-									setting.getNumber(), setting.getClass(), setting.getSubClass(),
-									setting.getProtocol(), setting.getNumEndpoints(), setting.getDescriptor()))
+										setting.getNumber(), setting.getClass(), setting.getSubClass(),
+										setting.getProtocol(), setting.getNumEndpoints(), setting.getDescriptor()))
 								for endpoint in setting:
 									self._logger.debug(
 										"  endpoint address: 0x{:02x}, attributes: 0x{:02x}, max packet size: {}".format(
-										endpoint.getAddress(), endpoint.getAttributes(),
-										endpoint.getMaxPacketSize()))
+											endpoint.getAddress(), endpoint.getAttributes(),
+											endpoint.getMaxPacketSize()))
 
 
 	def printer_factory(self, comm, port, baudrate, read_timeout, *args, **kwargs):
@@ -152,7 +159,9 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 			return None
 
 		self._comm = comm
-		serial_obj = flashforge.FlashForge(self, comm, self._printers[port]['vid'], self._printers[port]['did'], read_timeout=float(read_timeout))
+		serial_obj = flashforge.FlashForge(self, comm, port, self._printers[port]['vid'], self._printers[port]['did'],
+										   read_timeout=float(read_timeout))
+
 		return serial_obj
 
 
@@ -162,6 +171,13 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 		self.detect_printer()
 		printers = self._printers.keys()
 		return printers
+
+
+	def printer_capabilities(self, comm_instance, capability, enabled, already_defined, *args, ** kwargs):
+		""" OctoPrint hook - Called with printer capabilities
+		"""
+		if capability == "AUTOREPORT_TEMP":
+			self._serial_obj.disable_autotemp()
 
 
 	def get_extension_tree(self, *args, **kwargs):
@@ -184,6 +200,7 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 
 		thread = threading.Thread(target=serial_obj.keep_alive, name="FlashForge.Keep_Alive")
 		thread.daemon = True
+		serial_obj.enable_keep_alive(True)
 		thread.start()
 
 
@@ -210,9 +227,9 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 
 			self._logger.debug("rewrite_gcode(): gcode:{}, cmd:{}".format(gcode, cmd))
 
-			#TODO: detect printer state earlier in connection process and dont send M146, etc if the printer
+			# TODO: detect printer state earlier in connection process and dont send M146, etc if the printer
 			# is already busy when we connect
-			#TODO: filter M146 and other commands? when printing from SD because they cause comms to hang
+			# TODO: filter M146 and other commands? when printing from SD because they cause comms to hang
 
 			# allow a very limited set of commands while printing from SD to minimize problems...
 			if self._serial_obj.is_sd_printing() and gcode not in ["M24", "M25", "M26", "M27", "M105", "M112", "M114", "M117", "M400"]:
@@ -312,10 +329,12 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 
 	# Uploading files directly to internal SD card
 	def upload_to_sd(self, printer, filename, path, sd_upload_started, sd_upload_succeeded, sd_upload_failed, *args,
-						 **kwargs):
+					 **kwargs):
 
 		if not self._serial_obj:
 			return
+
+		# TODO: test printer status and do not proceed if not ready - eg homing after cancelling an SD print
 
 		def process_upload():
 			error = ""
@@ -324,6 +343,7 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 			self._upload_percent = 0
 			chunk_start_index = 0
 
+			self._serial_obj.enable_keep_alive(False)
 			self._serial_obj.makeexclusive(True)
 			error = "could not start tx"
 
@@ -374,9 +394,11 @@ class FlashForgePlugin(octoprint.plugin.SettingsPlugin,
 				self._logger.info("Upload failed: {}".format(error))
 				sd_upload_failed(filename, remote_name, 10)
 				self._serial_obj.makeexclusive(False)
+				self._serial_obj.enable_keep_alive(True)
 				raise flashforge.FlashForgeError(error)
 
 			self._serial_obj.makeexclusive(False)
+			self._serial_obj.enable_keep_alive(True)
 			# NB M23 select will also trigger a print on FlashForge
 			self._comm.selectFile("0:/user/%s\r\n" % remote_name, True)
 			# TODO: need to set the correct file size for the progress indicator
@@ -419,8 +441,8 @@ def __plugin_load__():
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information,
 		"octoprint.comm.transport.serial.factory": __plugin_implementation__.printer_factory,
 		"octoprint.comm.transport.serial.additional_port_names": __plugin_implementation__.get_additional_port_names,
+		"octoprint.comm.protocol.firmware.capabilities": __plugin_implementation__.printer_capabilities,
 		"octoprint.filemanager.extension_tree": __plugin_implementation__.get_extension_tree,
 		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.rewrite_gcode,
 		"octoprint.printer.sdcardupload": __plugin_implementation__.upload_to_sd
 	}
-
